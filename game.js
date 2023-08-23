c792dbaefe4667ac1af4364a70a16b825b13a9af
k-46 <kamalesh.a@kalvium.community>
2023-08-23 15:59:37 +0530

    made functional

--- game.js
+++ game.js
@@ -1,5 +1,65 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+const displayNum1 = document.getElementById("number1");
+const displayNum2 = document.getElementById("number2");
+const buttons = document.getElementById("buttons");
+const timer = document.getElementById("timer");
+let num1 ;
+let num2 ;
+let time =5;
+let score = 0;
+
+buttons.addEventListener("click",(e)=>{
+    compareNumbers(e.target.id);
+})
+
+// Comparing two numbers
+
+function compareNumbers(operation){
+    if(operation=="greater-than" && num1 > num2 || operation=="equal-to" && num1==num2 || operation=="lesser-than" && num1 < num2){
+        // console.log(operation)
+        displayRandomNumbers()
+        time = 5;
+        score++
+    }
+    else{
+        gameOver()
+    }
+    
+}
+
+function displayRandomNumbers(){
+     num1 = randomNumbers();
+     num2 = randomNumbers();
+    displayNum1.innerText=num1;
+    displayNum2.innerText=num2;
+}
+    
+function randomNumbers(){
+    let temp = Math.floor(Math.random()*100 + 1)
+    return temp
+}
+
+
+
 
 // Iteration 3: Make the options button functional
 
 // Iteration 4: Build a timer for the game
+
+function stopWatch(){
+    if(time<=0){
+        gameOver()
+    }
+    timer.innerText = time
+    time--
+}
+
+
+function gameOver(){
+    location.href = "gameover.html"
+    localStorage.setItem("score",score)
+    
+}
+
+setInterval(stopWatch,1000)
+displayRandomNumbers()
--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const playAgain = document.getElementById("play-again-button")
+playAgain.addEventListener("click",()=>{
+    location.href="game.html"
+})
+
+
+const finalScore = document.getElementById("score-board")
+
+finalScore.innerText=localStorage.getItem("score")
+console.log(localStorage.getItem("score"))
