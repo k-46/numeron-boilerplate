0e0c58ec456bdf1f031676ee6c434491bde9e461
k-46 <139456269+k-46@users.noreply.github.com>
2023-08-23 16:05:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 // Description: When the Play button is clicked the game page should be displayed.
 const play = document.getElementById("play-button");
 play.addEventListener("click",()=>{
-    window.open('./game.html')
+    window.open('game.html')
     console.log("alive")
 })
